0a9c18587707a25f4b969688e82addb7b5c30f62
Aksel Wester <aksel@wester.co>
2019-12-17 13:57:31 +0100

    Initial commit

---
+++ brewInstallConfig.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+brew install bash-completion
+brew install bat
+brew install gcc
+brew install gettext
+brew install git
+brew install gmp
+brew install helm
+brew install highlight
+brew install isl
+brew install jq
+brew install kotlin
+brew install kubernetes-helm
+brew install libmpc
+brew install lua
+brew install mono
+brew install mpfr
+brew install node
+brew install oniguruma
+brew install openssl
+brew install pcre2
+brew install readline
+brew install tig
+brew install watch
+
+brew cask install 1password
+brew cask install deckset
+brew cask install dropbox
+brew cask install emacs
+brew cask install firefox
+brew cask install google-chrome
+brew tap homebrew/cask-versions && brew cask install google-chrome-canary
+brew cask install intellij-idea
+brew cask install postman
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install slack
+brew cask install spotify
