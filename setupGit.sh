6d3a78bd9af07b9356ce6bba3b41872b4e536692
Aksel Wester <aksel@wester.co>
2023-12-19 22:54:13 +0100

    Legg til flere globale git-settings

--- setupGit.sh
+++ setupGit.sh
@@ -1,5 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 git config --global user.email "aksel@wester.co"
 git config --global user.name "Aksel Wester"
 git config --global pull.rebase false
+git config --global core.editor "emacs"
+git config --global init.defaultBranch "main"
+git config --global remote.origin.prune true
