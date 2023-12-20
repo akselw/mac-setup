0905c2ecc24f214a7496987bc461306df05a70ee
Aksel Wester <aksel@wester.co>
2023-12-20 09:08:20 +0100

    Legg til import av symbolichotkeys

--- setupShortcutsAndSettings.sh
+++ setupShortcutsAndSettings.sh
@@ -1,5 +1,10 @@
 # Global
 
+# Generert med `defaults export com.apple.symbolichotkeys - >symbolichotkeys.plist`
+# Tatt herfra https://stackoverflow.com/questions/34561758/is-there-a-way-to-change-keyboard-shortcuts-with-shell-in-os-x
+defaults import com.apple.symbolichotkeys symbolichotkeys.plist
+
+# Kan være at disse er unødvendige når den over kjører. Prøv det gjerne med en ny Mac
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "System Settings\\U2026" "@~,"
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Show Next Tab" "@~\\U2192"
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Show Previous Tab" "@~\\U2190"
